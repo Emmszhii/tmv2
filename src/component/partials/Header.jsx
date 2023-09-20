e99454905b975e2bd5d87d81ab76c2f1861ae615
Emmszhii <emmmanuelmanalo707@gmail.com>
2023-09-20 08:27:44 +0800

    Update emms

--- src/component/partials/Header.jsx
+++ src/component/partials/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <></>
-  )
-}
+    <>
+      <header>
+        <Logo />
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
