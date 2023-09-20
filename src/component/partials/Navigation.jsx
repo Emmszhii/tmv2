4f69e380e1acd70ee7d29aff21a83701b33f58b0
RoderickDM <RoderickDM>
2023-09-20 08:41:50 +0800

    font

--- src/component/partials/Navigation.jsx
+++ src/component/partials/Navigation.jsx
@@ -1,49 +1,37 @@
 import React from "react";
 import { StoreContext } from "../../store/StoreContext";
+import { setIsSettingsOpen, setIsToolsOpen } from "../../store/StoreAction";
 
 const Navigation = ({ menu, submenu = null, val }) => {
   const { store, dispatch } = React.useContext(StoreContext);
+
+  const handleDropDownTools = () => {
+    dispatch(setIsToolsOpen(!store.setIsToolsOpen));
+  };
 
   return (
     <div className="px-2 py-4 bg-tm-gradient h-full">
       <ul className="custom__scroll">
         <li className="nav__link  has__dropdown">
           <button
-            className={`${menu === "settings" ? "bg-[#436c8a]" : ""}`}
-            onClick={() => handleDropDownSetting()}
+            className={`${menu === "tools" ? "bg-[#436c8a]" : ""}`}
+            onClick={() => handleDropDownTools()}
           >
             <div className="flex justify-between items-center w-full">
               <div className="flex gap-3 items-center ">
                 <BsGear className="text-lg" /> Tools
               </div>
               <PiCaretRight
-                className={!store.isSettingsOpen ? "rotate-0" : "rotate-90"}
+                className={!store.setIsToolsOpen ? "rotate-0" : "rotate-90"}
               />
             </div>
           </button>
         </li>
-        <div className={store.isSettingsOpen ? "showdropdown" : "hidden"}>
+        <div className={store.setIsToolsOpen ? "showdropdown" : "hidden"}>
           <ul className="ml-9 ">
             <li
               className={` ${
-                submenu === "settingsDepartment"
-                  ? "bg-[#436c8a]/80 rounded-md"
-                  : ""
-              }`}
-            >
-              <Link
-                to={`${urlRolePath}/settings/department`}
-                className={`text-white border-l-2 hover:!border-accent duration-150 hover:!border-l-2 border-transparent pl-2 w-fit inline-block py-1 ${
-                  submenu === "settingsDepartment" ? "active__submenu" : ""
-                }`}
-              >
-                Department
-              </Link>
-            </li>
-
-            <li
-              className={` ${
-                submenu === "settingsSampleOtp"
+                submenu === "toolsWorkLoadChart"
                   ? "bg-[#436c8a]/80 rounded-md"
                   : ""
               }`}
@@ -54,7 +42,7 @@ const Navigation = ({ menu, submenu = null, val }) => {
                   submenu === "settingsSampleOtp" ? "active__submenu" : ""
                 }`}
               >
-                Sample Otp
+                Sample
               </Link>
             </li>
           </ul>
