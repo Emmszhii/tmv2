1883dd5c4bc87e6c6211e79ed3ee37c0a6751dc8
RoderickDM <RoderickDM>
2023-09-20 09:10:40 +0800

    helpers

--- src/component/partials/Navigation.jsx
+++ src/component/partials/Navigation.jsx
@@ -6,12 +6,18 @@ import {
   setIsTimeEntryOpen,
   setIsToolsOpen,
 } from "../../store/StoreAction";
+import { devNavUrl } from "../helpers/functions-general";
 
 const Navigation = ({ menu, submenu = null, val }) => {
   const { store, dispatch } = React.useContext(StoreContext);
+  const urlRolePath = `${devNavUrl}`;
 
   const handleDropDownTools = (e) => {
     dispatch(setIsToolsOpen(!store.isToolsOpen));
+  };
+
+  const handleDropDownSettings = (e) => {
+    dispatch(setIsSettingsOpen(!store.isSettingsOpen));
   };
   //   const handleDropDownTimeEntry = (e) => {
   //     dispatch(setIsTimeEntryOpen(!store.isTimeEntryOpen));
@@ -119,19 +125,84 @@ const Navigation = ({ menu, submenu = null, val }) => {
         </li>
         <li className="nav__link  has__dropdown">
           <button
-            className={`${menu === "Client" ? "bg-[#436c8a]" : ""}`}
-            // onClick={() => handleDropDownStaff()}
+            className={`${menu === "client" ? "bg-[#436c8a]" : ""}`}
+            // onClick={() => handleDropDownClient()}
           >
             <div className="flex justify-between items-center w-full">
               <div className="flex gap-3 items-center ">
-                <BsGear className="text-lg" /> Staff
+                <BsGear className="text-lg" /> Client
               </div>
               {/* <PiCaretRight
-                className={!store.isTimeEntryOpen ? "rotate-0" : "rotate-90"}
+                className={!store.isClientOpen ? "rotate-0" : "rotate-90"}
               /> */}
             </div>
           </button>
         </li>
+        <li className="nav__link  has__dropdown">
+          <button
+            className={`${menu === "settings" ? "bg-[#436c8a]" : ""}`}
+            onClick={() => handleDropDownSettings()}
+          >
+            <div className="flex justify-between items-center w-full">
+              <div className="flex gap-3 items-center ">
+                <BsGear className="text-lg" /> Settings
+              </div>
+              <PiCaretRight
+                className={!store.isClientOpen ? "rotate-0" : "rotate-90"}
+              />
+            </div>
+          </button>
+        </li>
+        <div className={store.isSettingsOpen ? "showdropdown" : "hidden"}>
+          <ul className="ml-9 ">
+            <li
+              className={` ${
+                submenu === "toolsWorkLoadChart"
+                  ? "bg-[#436c8a]/80 rounded-md"
+                  : ""
+              }`}
+            >
+              <Link
+                to={`${urlRolePath}/tools/workloadchart`}
+                className={`text-white border-l-2 hover:!border-accent duration-150 hover:!border-l-2 border-transparent pl-2 w-fit inline-block py-1 ${
+                  submenu === "toolsWorkLoadChart" ? "active__submenu" : ""
+                }`}
+              >
+                WorkLoad Chart
+              </Link>
+            </li>
+            <li
+              className={` ${
+                submenu === "settingsAccessLevel"
+                  ? "bg-[#436c8a]/80 rounded-md"
+                  : ""
+              }`}
+            >
+              <Link
+                to={`${urlRolePath}/settings/accesslevel`}
+                className={`text-white border-l-2 hover:!border-accent duration-150 hover:!border-l-2 border-transparent pl-2 w-fit inline-block py-1 ${
+                  submenu === "settingsAccessLevel" ? "active__submenu" : ""
+                }`}
+              >
+                Access Level
+              </Link>
+            </li>
+            <li
+              className={` ${
+                submenu === "settingsUsers" ? "bg-[#436c8a]/80 rounded-md" : ""
+              }`}
+            >
+              <Link
+                to={`${urlRolePath}/settings/users`}
+                className={`text-white border-l-2 hover:!border-accent duration-150 hover:!border-l-2 border-transparent pl-2 w-fit inline-block py-1 ${
+                  submenu === "settingsUsers" ? "active__submenu" : ""
+                }`}
+              >
+                Users
+              </Link>
+            </li>
+          </ul>
+        </div>
       </ul>
     </div>
   );
