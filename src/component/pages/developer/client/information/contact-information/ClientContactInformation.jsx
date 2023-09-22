ed0e423ea0b8737eedff0a934b4de79bb5b069a2
Emmszhii <emmmanuelmanalo707@gmail.com>
2023-09-22 12:53:48 +0800

    update emms

---
+++ src/component/pages/developer/client/information/contact-information/ClientContactInformation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ClientContactInformation = () => {
+  return (
+    <div>ClientContactInformation</div>
+  )
+}
+
+export default ClientContactInformation
--- src/component/partials/MainFooter.jsx
+++ src/component/partials/MainFooter.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const MainFooter = () => {
   return (
-    <div className=" p-1 fixed bottom-0 mx-auto z-20 w-full before:border-t-2 before:content-[''] before:bg-gray-50 before:z-10 before:absolute before:top-0 before:left-0 before:h-full before:w-full after:border-t-2 after:content-[''] after:bg-gray-50 after:z-10 after:absolute after:top-0 after:right-0 after:h-full after:w-full">
-      <p className="text-[10px] leading-none m-0 relative z-30">
+    <div className=" p-1 py-2 fixed bottom-0 left-0 w-full text-center z-20 before:border-t-2 before:content-[''] before:bg-gray-50 before:z-10 before:absolute before:top-0 before:-left-24 before:h-full before:w-full after:border-t-2 after:content-[''] after:bg-gray-50 after:z-10 after:absolute after:top-0 after:right-0 after:h-full after:w-full">
+      <p className="text-[10px] leading-none m-0 relative z-30 pl-48">
         © 2023 All Rights Reserved. | Powered by
-        <span className="font-bold text-alert_darker/90">
+        <span className="font-bold text-alert/90">
           {" "}
           Frontline Business Solutions, Inc..
         </span>
