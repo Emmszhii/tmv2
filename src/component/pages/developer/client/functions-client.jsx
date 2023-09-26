86d802e9304364099d4af2c50bb6cca5b4f003a6
RoderickDM <RoderickDM>
2023-09-26 09:22:59 +0800

    update ni rhico

--- src/component/pages/developer/client/functions-client.jsx
+++ src/component/pages/developer/client/functions-client.jsx
@@ -1,17 +1,16 @@
 export const getClientCountRecord = (client) => {
-    let active = 0;
-    let inactive = 0;
-  
-    const resultActive = client?.data.filter(
-      (acItem) => acItem.client_is_active === 1
-    );
-    active = resultActive?.length;
-  
-    const resultInactive = client?.data.filter(
-      (inacItem) => inacItem.client_is_active === 0
-    );
-    inactive = resultInactive?.length;
-  
-    return { active, inactive };
-  };
-  
+  let active = 0;
+  let inactive = 0;
+
+  const resultActive = client?.data.filter(
+    (acItem) => acItem.client_is_active === 1
+  );
+  active = resultActive?.length;
+
+  const resultInactive = client?.data.filter(
+    (inacItem) => inacItem.client_is_active === 0
+  );
+  inactive = resultInactive?.length;
+
+  return { active, inactive };
+};
